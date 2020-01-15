bdce3f4ae82836c4aef692ee2ca570462f4592ab
edgarhermenegildo <edgarhermenegildo@gmail.com>
2020-01-14 23:21:18 -0300

    Ajustes do menu.

--- program.py
+++ program.py
@@ -1,2 +1,13 @@
 from libs.packeges import interface
+from clint.textui import colored
+import os
 
+lista = ['Cadastrar Cliente', 'Consultar Cliente', 'Sair']
+
+while True:
+    opc = interface.menu(lista)
+    if opc == 3:
+        interface.titulo('Saindo do Sistema.')
+        break
+    else:
+        print(colored.red('ERRO! Digite uma opção valida.'))
--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ lista = ['Cadastrar Cliente', 'Consultar Cliente', 'Sair']
 while True:
     opc = interface.menu(lista)
     if opc == 3:
-        interface.titulo('Saindo do Sitema.')
+        interface.titulo('Saindo do Sistema.')
         break
     else:
         print(colored.red('ERRO! Digite uma opção valida.'))
