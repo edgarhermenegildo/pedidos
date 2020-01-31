ea598e7554079d676a2bba525a74af53359faf0a
edgarhermenegildo <edgarhermenegildo@gmail.com>
2020-01-31 17:01:51 -0300

    Arquivo JSON

--- libs/config/connect.py
+++ libs/config/connect.py
@@ -1,9 +1,17 @@
 import pymysql      # Importando biblioteca pymysql, para conectar no bancom de dados.
-import dados        # Importando o modulo dados (local).
+import json        # Importando o modulo dados (local).
 
-connect = pymysql.connect(
-    host = '172.16.0.100',
-    user = dados.userdb,
-    passwd = dados.password,
-    db = dados.database
-    )
+# Ler arquivo JSON
+try:
+    arquivo_json = open('config.json', 'r')
+    dados_json = json.load(arquivo_json)
+except:
+    print('Arquivo não encontrado.')
+#connect = pymysql.connect(
+#    host = '172.16.0.100',
+#    user = dados.userdb,
+#    passwd = dados.password,
+#    db = dados.database
+#    )
+
+print(dados_json)
