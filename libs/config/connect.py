7fd0a150f6fcaf9599bf494212cc0dbb3ea4434f
edgarhermenegildo <edgarhermenegildo@gmail.com>
2020-02-01 00:27:07 -0300

    conexão com o banco de dados.

--- libs/config/connect.py
+++ libs/config/connect.py
@@ -1,17 +1,19 @@
 import pymysql      # Importando biblioteca pymysql, para conectar no bancom de dados.
 import json        # Importando o modulo dados (local).
 
+
 # Ler arquivo JSON
 try:
-    arquivo_json = open('config.json', 'r')
+    arquivo_json = open('/home/edgar/projetos/pedidos/libs/config/config.json', 'r')
     dados_json = json.load(arquivo_json)
 except:
     print('Arquivo não encontrado.')
-#connect = pymysql.connect(
-#    host = '172.16.0.100',
-#    user = dados.userdb,
-#    passwd = dados.password,
-#    db = dados.database
-#    )
-
-print(dados_json)
+else:
+    conexao = pymysql.connect(
+        host='172.16.0.100',
+        user=dados_json['userdb'],
+        passwd=dados_json['password'],
+        db=dados_json['database']
+        )
+finally:
+    arquivo_json.close()
--- program.py
+++ program.py
@@ -1,13 +1,31 @@
 from libs.packeges import interface
+from libs.config import connect
 from clint.textui import colored
+import getpass
 import os
 
 lista = ['Cadastrar Cliente', 'Consultar Cliente', 'Sair']
-
+flag = cont = 0
 while True:
-    opc = interface.menu(lista)
-    if opc == 3:
-        interface.titulo('Saindo do Sistema.')
+    login = str(input('Login: ')).strip().lower()
+    passwd = getpass.getpass('Digite sua senha: ')
+    cursor = connect.conexao.cursor()
+    cursor.execute('SELECT username, password FROM tbl_staff')
+    resultado = cursor.fetchall()
+    for Login, Passwd in resultado:
+        if Login == login and Passwd == passwd:
+            flag += 1
+            print('Login efetuado com sucesso.')
+            while True:
+                opc = interface.menu(lista)
+                if opc == 3:
+                    interface.titulo('Saindo do Sistema.')
+                    break
+                else:
+                    print(colored.red('ERRO! Digite uma opção valida.'))
+    cont += 1
+    if flag == 1 or cont == 3:
         break
-    else:
-        print(colored.red('ERRO! Digite uma opção valida.'))
+    print('Login ou tenha invalido.')
+
+
